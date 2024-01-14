26838c7e381188e5839339aa0140286a1733eb90
vladyslavbezruk <vladyslavbezruk@yahoo.com>
2024-01-14 18:36:00 +0200

    Added code to start bot

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-# coding: utf-8
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+
+from configurations.settings import *
+
+loop = asyncio.get_event_loop()
+bot = Bot(BOT_TOKEN, parse_mode="HTML")
+dp = Dispatcher(loop=loop)
+
+
+async def main():
+    from bot.handlers import dp
+
+    logging.basicConfig(level=logging.INFO)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
