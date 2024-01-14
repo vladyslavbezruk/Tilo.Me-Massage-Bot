6a506bee54960dec0885359c1eee7e54dcb96e96
vladyslavbezruk <vladyslavbezruk@yahoo.com>
2024-01-14 20:23:04 +0200

    Added assets.

--- assets/assets.py
+++ assets/assets.py
@@ -1,1 +1,13 @@
+command_start = "start"
 
+start_answer_message = "Як ми можемо вам допомогти?"
+
+message_cancel = "Скасувати"
+
+cancel_answer_message = "Скасовано"
+
+message_enroll = "Записатись"
+
+message_view_enrolls = "Переглянути мої записи"
+
+start_keyboard_answer_message = "Оберіть дію"
