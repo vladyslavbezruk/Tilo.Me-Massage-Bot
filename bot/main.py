a8454b580087e21488911d986c71670df6d1d761
vladyslavbezruk <vladyslavbezruk@yahoo.com>
2024-01-16 20:16:47 +0200

    Fixed

--- bot/main.py
+++ bot/main.py
@@ -2,10 +2,15 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 from configurations.settings import *
+import assets.languages as languages
+import assets.assets as assets
 
 loop = asyncio.get_event_loop()
 bot = Bot(BOT_TOKEN, parse_mode="HTML")
 dp = Dispatcher(loop=loop)
+
+languages.load_languages()
+assets.set_language('ua')
 
 async def main():
     from bot.handlers import dp
