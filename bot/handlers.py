2496bc3820855daa7f9124203a492d5c5feb3c4b
Vladyslav Bezruk <vladyslavbezruk@yahoo.com>
2024-02-29 23:24:39 +0200

    Fixed enroll handler. Added enroll_options handler.

--- bot/handlers.py
+++ bot/handlers.py
@@ -3,23 +3,29 @@ from aiogram.types import *
 from assets.assets import *
 from bot.main import dp
 from bot.start_keyboard import start_keyboard
-
+from bot.enroll_keyboard import enroll_keyboard
 
 @dp.message(Command(command_start))
 async def echo(message: Message):
-    await message.answer(text=start_answer_message, reply_markup=start_keyboard)
+    await message.reply(text=start_answer_message, reply_markup=start_keyboard)
 
 
 @dp.message(lambda message: message.text == message_cancel)
 async def echo(message: Message):
-    await message.answer(text=cancel_answer_message, reply_markup=ReplyKeyboardRemove())
+    await message.reply(text=cancel_answer_message, reply_markup=ReplyKeyboardRemove())
 
 
 @dp.message(lambda message: message.text == message_enroll)
 async def echo(message: Message):
-    await message.answer(text="Coming soon ...", reply_markup=ReplyKeyboardRemove())
+    await message.reply(text=enroll_answer_message, reply_markup=enroll_keyboard)
 
 
 @dp.message(lambda message: message.text == message_view_enrolls)
 async def echo(message: Message):
-    await message.answer(text="Coming soon ...", reply_markup=ReplyKeyboardRemove())
+    await message.reply(text="Coming soon ...", reply_markup=ReplyKeyboardRemove())
+
+@dp.message(lambda message: message.text in enroll_options)
+async def echo(message: Message):
+    category = message.text
+
+    await message.reply(text=chosen_enroll_options_answer_message + category)
