60245d313da14ff6e29423dd0edb66752bb70622
Vladyslav Bezruk <vladyslavbezruk@yahoo.com>
2024-03-04 18:29:24 +0200

    Fixed.

--- utils/files.py
+++ utils/files.py
@@ -2,11 +2,11 @@ import codecs
 import json
 
 
-def load_file(FilePath):
-    with codecs.open(FilePath, encoding='utf-8') as file:
+def load_file(file_path):
+    with codecs.open(file_path, encoding='utf-8') as file:
         return json.loads(file.read())
 
 
-def save_file(data, FilePath):
-    with codecs.open(FilePath, "w", encoding='utf-8') as file:
+def save_file(data, file_path):
+    with codecs.open(file_path, "w", encoding='utf-8') as file:
         json.dump(data, file)
