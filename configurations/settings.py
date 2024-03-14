51efc2b707f633ef13a9aab225062fe94c7d0f40
Vladyslav Bezruk <vladyslavbezruk@yahoo.com>
2024-03-14 20:41:24 +0200

    Fixed.

--- configurations/settings.py
+++ configurations/settings.py
@@ -1,7 +1,7 @@
 import os
 
-BOT_TOKEN = "6925007502:AAHGEGOuUgTNiS995sXZA0gde3mPa4bsAbw"
-BOT_NAME = "Tilo.Me-Massage-Bot"
+BOT_TOKEN = '6925007502:AAHGEGOuUgTNiS995sXZA0gde3mPa4bsAbw'
+BOT_NAME = 'Tilo.Me-Massage-Bot'
 PROJECT_DIR = os.getcwd()
 
 while os.path.basename(PROJECT_DIR) != BOT_NAME:
